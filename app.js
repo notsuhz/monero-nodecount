61dcca6f576ec29204f21837c74c8a3feaa5fea8
Suhaimi Amir <suhaimi.hz@gmail.com>
2017-06-04 09:57:34 +0800

    hello world

---
+++ app.js
@@ -0,0 +1,125 @@
+"use strict";
+
+const config = require('./config')
+const portscanner = require('portscanner')
+const exec = require('child_process').exec;
+const geoip = require('geoip-lite');
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(config.db_name, config.db_username, config.db_password);
+
+const Nodes = sequelize.define('monero_nodes', {
+    id: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        autoIncrement: true
+    },
+    ip: {
+        type: Sequelize.STRING
+    },
+    port: {
+        type: Sequelize.INTEGER(11).UNSIGNED
+    },
+    first_seen: {
+        type: Sequelize.DATE
+    },
+    last_seen: {
+        type: Sequelize.DATE
+    },
+    country: {
+        type: Sequelize.STRING(3)
+    },
+    status: {
+        type: Sequelize.INTEGER
+    }
+}, {
+    freezeTableName: true // Model tableName will be the same as the model name
+});
+
+Nodes.sync();
+
+const peer_list = exec(`${config.monero_daemon} print_pl`);
+let result = '';
+
+peer_list.stdout.on('data', function(data) {
+    result += data;
+});
+
+peer_list.on('close', function(data) {
+    result = result.split("\n");
+
+    for (let i = 0; i <= result.length - 1; i++) {
+
+        //we dont want to scan thousand of ip at the same time, right?
+        setTimeout(function(i) {
+
+            const line = result[i].toString().split(/\s+/);
+
+            if (line != '') {
+                const host = line[2].split(':');
+                const ip = host[0];
+                const port = host[1];
+                //console.log(geoip.lookup(ip).country);
+
+                if (!ValidateIPaddress(ip)) { return }
+
+                portscanner.checkPortStatus(port, ip).then(function(status) {
+                    //console.log(status);
+
+                    if (status == 'open') { //else closed
+                        Nodes.findOne({
+                            where: {
+                                ip: ip
+                            }
+                        }).then(node => {
+                            if (node) {
+                                node.updateAttributes({
+                                    last_seen: sequelize.fn('NOW'),
+                                    country: geoip.lookup(ip).country,
+                                    status: 1
+                                })
+                            } else {
+                                Nodes.create({
+                                    ip: ip,
+                                    port: port,
+                                    first_seen: sequelize.fn('NOW'),
+                                    last_seen: sequelize.fn('NOW'),
+                                    country: geoip.lookup(ip).country,
+                                    status: 1
+                                })
+                            }
+
+                        });
+                    } else if (status == 'closed') {
+                        Nodes.findOne({
+                            where: {
+                                ip: ip
+                            }
+                        }).then(node => {
+                            if (node) {
+                                node.updateAttributes({
+                                    status: 0
+                                })
+                            }
+                        });
+                    }
+
+                }).catch(function(e) {
+                    console.log(e);
+                });
+
+            }
+
+        }, config.scan_interval * i, i);
+
+    }
+
+});
+
+
+function ValidateIPaddress(ipaddress) {
+  if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ipaddress)) {
+    return (true)
+  }
+  return (false)
+}
---
+++ config.sample.js
@@ -0,0 +1,7 @@
+module.exports = {
+    db_name: 'xmrmy',
+    db_username: 'xmrmy',
+    db_password: 'xmrmy',
+    scan_interval: 1000, //how many second (in milisecond) to wait to scan for next ip in the peer lists
+    monero_daemon: '/home/monero/bin/monerod' //path to monero daemon
+}
